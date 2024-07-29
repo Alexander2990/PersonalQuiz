ddcf235d18e9dfc5ece94f484793e5afebcaf36d
Alexander <Alexander29_90@icloud.com>
2024-07-29 18:10:48 +0500

    Created a question model

---
+++ PersonalQuiz/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by Александр on 29.07.2024.
+//
+
+import Foundation
---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Александр on 29.07.2024.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
